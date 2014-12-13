aa59a5545e5d12f10c3ac2b9de7eb4b0ea615a3b
Opvolger <Opvolger@gmail.com>
2014-12-13 17:53:01 +0100

    version 1.1.0

--- addon.py
+++ addon.py
@@ -1,65 +1,61 @@
 '''
-    Uitzendinggemist(NPO)
+    RTLxl
     ~~~~~
 
-    An XBMC addon for watching uzg
+    An XBMC addon for watching RTLxl
 
     :copyright: (c) 2012 by Jonathan Beluch (Documentary.net xbmc addon)    
     :license: GPLv3, see LICENSE.txt for more details.
     
     based on: https://github.com/jbeluch/plugin.video.documentary.net
 
-    uzg = Made/(changed Jonathans code) by Bas Magre (Opvolger)
+    RTLxl = Made/(changed Jonathans code) by Bas Magre (Opvolger)
     
 '''
 from xbmcswift2 import Plugin, SortMethod
-from resources.lib.uzg import  get_overzicht, get_items_uitzending, get_url , get_ondertitel
-import time
-import xbmcplugin
+import resources.lib.rtlxl
 
-PLUGIN_NAME = 'uzg'
-PLUGIN_ID = 'plugin.video.uzg'
+PLUGIN_NAME = 'RTLxl'
+PLUGIN_ID = 'plugin.video.rtlxl'
 plugin = Plugin(PLUGIN_NAME, PLUGIN_ID, __file__)
+
+rtlxl = resources.lib.rtlxl.RtlXL()
+mp4low = plugin.get_setting( "mp4low",bool )
 
 @plugin.route('/')
 def index():
     ##main, alle shows
     items = [{
-        'path': plugin.url_for('show_afleveringen', nebo_id=item['nebo_id']),
-        'label': item['title'],
+        'path': plugin.url_for('show_keuze', url=item['url']),
+        'label': item['label'],
         'thumbnail': item['thumbnail'],
-    } for item in get_overzicht()]    
+    } for item in rtlxl.get_overzicht()]    
     return items
 
-@plugin.route('/afleveringen/<nebo_id>/')
-def show_afleveringen(nebo_id):
-    ##alleen afleveringen weergeven
-    return show_items(get_items_uitzending(nebo_id))
-
-@plugin.route('/lectures/<url>/')
-def play_lecture(url):
-	plugin.set_resolved_url(get_url(url))	
-	waarde = plugin.get_setting( "subtitle",bool )
-	if (waarde):
-		add_subtitlesstream(get_ondertitel(url))
-
-def add_subtitlesstream(subtitles):
-	player = xbmc.Player()
-	for _ in xrange(30):
-		if player.isPlaying():
-			break
-		time.sleep(1)
-	else:
-		raise Exception('No video playing. Aborted after 30 seconds.')
-	player.setSubtitles(subtitles)
-	player.setSubtitleStream(1)
-
-	
-def show_items(opgehaaldeitemsclass):
-    '''Lists playable videos for a given category url.'''
+@plugin.route('/keuze/<url>/')
+def show_keuze(url):
+    ##keuze tussel alles of alleen afleveringen (wel fijn bij RTL Nieuws)
     items = [{
-        'path': plugin.url_for('play_lecture', url=item['playerid']),
+        'path': plugin.url_for('show_'+item['keuze'], url=item['url']),
         'label': item['title'],
+        'selected': item['selected'],
+    } for item in rtlxl.get_categories(url)]    
+    return items
+
+@plugin.route('/keuze/afleveringen/<url>/')
+def show_afleveringen(url):
+    ##alleen afleveringen weergeven
+    return show_items(rtlxl.get_items(url, False, mp4low))
+
+@plugin.route('/keuze/alles/<url>/')
+def show_alles(url):    
+    ##alles weergeven
+    return show_items(rtlxl.get_items(url, True, mp4low))
+    
+def show_items(opgehaaldeitemsclass):
+    items = [{
+        'path': item['path'],
+        'label': item['label'],
         'thumbnail': item['thumbnail'],
         'is_playable': True,
         'info': {
