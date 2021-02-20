bfd0e0442f973a105171b3f2a9c588ed19a448c7
Bas M <opvolger@gmail.com>
2021-02-20 14:19:14 +0100

    matrix heeft dit nodig + alvast andere aanroep in code

--- resources/lib/npoapihelpers.py
+++ resources/lib/npoapihelpers.py
@@ -2,6 +2,7 @@ import json
 import re
 import sys
 
+
 from resources.lib.jsonhelper import ToJsonObject
 
 if (sys.version_info[0] == 3):
@@ -16,16 +17,26 @@ else:
 
 class NpoHelpers():
 
-    def get_json_data(self, url, data=None):
+    def get_json_data(self, url, data=None, headers=None, add_headers=False):
         req = Request(url)
         req.add_header(
             'User-Agent',
             'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36')
         req.add_header('Content-Type', 'application/json; charset=utf-8')
         req.add_header('ApiKey', '07896f1ee72645f68bc75581d7f00d54')
+        if (headers):
+            for key in headers:
+                req.add_header(key, headers[key])
         response = urlopen(req, data)
         link = response.read()
         response.close()
+        if (add_headers):
+            response.headers.headers
+            if (sys.version_info[0] == 3):
+                response_headers = response.getheaders()
+            else:
+                response_headers = response.headers.headers
+            return json.loads(link), response_headers
         return json.loads(link)
 
     def get_subtitles(self, whatson_id):
@@ -40,11 +51,19 @@ class NpoHelpers():
         return url
 
     def get_play_url(self, whatson_id):
-
+        # video = self.get_stream_url(whatson_id)
+        # if (video['stream']['type'] == 'application/dash+xml'):
+        #     xcdata_value = video['stream']['keySystemOptions'][0]['options']['httpRequestHeaders']['x-custom-data']
+        #     license_url = video['stream']['keySystemOptions'][0]['options']['licenseUrl']
+        #     item = {
+        #         'url': video['stream']['src'],
+        #         'drm': True,
+        #         'license_key': license_url + '|Content-Type=&User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3041.0 Safari/537.36&x-custom-data=%s|R{SSM}|' % xcdata_value
+        #     }
+        #     return item
         url = 'https://start-api.npo.nl/media/'+whatson_id+'/stream'
         data_dash = self.get_json_data(
             url, NpoHelpers.__get_video_request_data('dash'))
-
         if (data_dash['drm']):
             data_dash['license_key'] = data_dash['licenseServer'] + \
                 '|X-Custom-Data=' + data_dash['licenseToken'] + '|R{SSM}|'
@@ -53,10 +72,32 @@ class NpoHelpers():
             return self.get_json_data(url, NpoHelpers.__get_video_request_data('hls'))
         return data_dash
 
+    # werkt ook, maar met GEO blokking en omslachtig en kwaliteit is niet beter.
+    def get_stream_url(self, whatson_id):
+        # https://github.com/RandomIntermition/k4y108837s/blob/30dc07ee017978dafbe77ad651a9cf0a9cfc267d/HAX/18.CocoJoe/plugin.video.catchuptvandmore/resources/lib/channels/nl/npo.py
+        headers = {'X-Requested-With': 'XMLHttpRequest'}
+        data, cookies = self.get_json_data("https://www.npostart.nl/api/token", headers=headers, add_headers=True)
+        for cookie in cookies:
+            if (sys.version_info[0] == 3):
+                if cookie[1].startswith("npo_session="):
+                    session_token = cookie[1]
+            else:
+                if cookie.startswith("Set-Cookie: npo_session="):
+                    session_token = cookie[12:].split('\r\n')[0]
+
+        api_token = data['token']        
+        payload = json.dumps({"_token": api_token}).encode('utf-8')
+
+        headers = { "Cookie": session_token }
+        
+        resp2 = self.get_json_data("https://www.npostart.nl/player/{0}".format(whatson_id), data=payload, headers=headers)
+        token_id = resp2['token']
+        # video uitvraag
+        video = self.get_json_data("https://start-player.npo.nl/video/{}/streams?profile=dash-widevine&quality=npo&tokenId={}&streamType=broadcast&mobile=0&ios=0&isChromecast=0".format(whatson_id, token_id))
+        return video
+
     @staticmethod
     def __get_video_request_data(profile):
-        # https://github.com/RandomIntermition/k4y108837s/blob/30dc07ee017978dafbe77ad651a9cf0a9cfc267d/HAX/18.CocoJoe/plugin.video.catchuptvandmore/resources/lib/channels/nl/npo.py
-        # 'https://start-player.npo.nl/video/%s/streams?profile=dash-widevine&quality=npo&tokenId=%s&streamType=broadcast&mobile=0&ios=0&isChromecast=0'
         data = ToJsonObject()
         data.profile = profile
         data.options = ToJsonObject()
