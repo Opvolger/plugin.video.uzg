32fa5214f8aef62acd1de1860bb8c3a58bdcd9a5
Bas Magré <Opvolger@gmail.com>
2023-12-10 13:48:34 +0100

    Eerst opzet... nog niet alles getest

--- addon.py
+++ addon.py
@@ -10,25 +10,13 @@ import xbmcaddon
 import xbmcgui
 import xbmcplugin
 import inputstreamhelper
-import time
-import json
 import sys
 
 from resources.lib.uzg import Uzg
+from resources.lib.npoapiclasses import AddonItems
 
-if (sys.version_info[0] == 3):
-    # For Python 3.0 and later
-    from urllib.parse import urlencode
-    from urllib.parse import parse_qsl
-    import storageserverdummy as StorageServer
-else:
-    # Fall back to Python 2's urllib2
-    from urllib import urlencode
-    from urlparse import parse_qsl
-    try:
-        import StorageServer
-    except:
-        import storageserverdummy as StorageServer
+from urllib.parse import urlencode
+from urllib.parse import parse_qsl
 
 
 PROTOCOL = 'mpd'
@@ -41,45 +29,49 @@ uzg = Uzg()
 
 _url = sys.argv[0]
 _handle = int(sys.argv[1])
-# (Your plugin name, Cache time in hours)
-_cache = StorageServer.StorageServer(PLUGIN_ID, 24)
 _addon = xbmcaddon.Addon()
 
 # het in elkaar klussen van een url welke weer gebruikt word bij router
-
-
-def get_url(**kwargs):
+def getUrl(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs))
-
 
 def setMediaView():
     # juiste skin selecteren alleen voor confluence maar die gebruik ik prive nog steeds
     try:
-        kodiVersion = xbmc.getInfoLabel('System.BuildVersion').split()[0]
-        kodiVersion = kodiVersion.split('.')[0]
         skinTheme = xbmc.getSkinDir().lower()
         if 'onfluence' in skinTheme:
             xbmc.executebuiltin('Container.SetViewMode(504)')
     except:
         pass
 
-
-def home_menu():
+def homeMenu():
     xbmcplugin.setPluginCategory(_handle, 'NPO Start')
-    for category in ['Live kanalen', 'Zoeken', 'Letters']:
+    for category in ['Live kanalen', 'Zoeken']:
+    #for category in ['Live kanalen', 'Zoeken', 'Letters']:
         list_item = xbmcgui.ListItem(label=category)
-        url = get_url(action='keuze', keuze=category)
+        url = getUrl(action='keuze', keuze=category)
         is_folder = True
         xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
     xbmcplugin.endOfDirectory(_handle)
 
+def listSeasons(slug):
+    items = uzg.getSeasons(slug)
+    addItems(items)
 
-def list_keuze(keuze):
+def listEpisodesofSerie(guid):
+    items = uzg.getEpisodesOfSerie(guid)
+    addItems(items)
+
+def listEpisodesofSeason(guid):
+    items = uzg.getEpisodesOfSeason(guid)
+    addItems(items)
+
+def listKeuze(keuze):
     xbmcplugin.setPluginCategory(_handle, keuze)
     xbmcplugin.setContent(_handle, 'videos')
 
     if(keuze == 'Live kanalen'):
-        list_livestreams()
+        listLiveStreams()
 
     elif (keuze == 'Zoeken'):
         dialog = xbmcgui.Dialog()
@@ -87,142 +79,70 @@ def list_keuze(keuze):
         input_dialog = dialog.input(_addon.getLocalizedString(32004), type=xbmcgui.INPUT_ALPHANUM)
         if input_dialog:
             # als hij hier niet komt is hij geannuleerd of afgebroken
-            franchises = _cache.cacheFunction(uzg.getQueryPage, input_dialog)
-        displays_franchises(franchises)
-
-    elif(keuze == 'Letters'):
-        list_overzicht()
+            franchises = uzg.getQueryPage(input_dialog)
+        addItems(franchises)
 
 
-def list_livestreams():
+def listLiveStreams():
     videos = list()
-    videos = _cache.cacheFunction(uzg.getChannels)
+    videos = uzg.getChannels()
     if videos:
-        add_video_items(videos)
+        addItems(videos)
 
     xbmcplugin.endOfDirectory(_handle)
 
 
-def list_letter(letter):
-    # ophalen franchises aan de hand van een "letter"
-    franchises = _cache.cacheFunction(uzg.getAZPage, letter)
-    displays_franchises(franchises)
-
-
-def displays_franchises(franchises):
-    # displays franchises
-    if franchises:
-        for franchise in franchises:
-            list_item = xbmcgui.ListItem(label=franchise['label'])
-            list_item.setArt(franchise['art'])
-            list_item.setInfo('video', franchise['video'])
-            url = get_url(action='episodes', link=franchise['apilink'])
-            is_folder = True
-            xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-        xbmcplugin.addSortMethod(
-            _handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-
+def addItems(addonitems: list[AddonItems]):
+    if addonitems:
+        if addonitems[0].kodiInfo.isPlayable:
+            xbmcplugin.setContent(_handle, 'videos')
+            xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_DATE)
+            xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+        for addonitem in addonitems:
+            # weergave video's die kan direct onder een franchise zijn of vanaf een sub-level "seasons"
+            list_item = xbmcgui.ListItem(label=addonitem.kodiInfo.label)
+            list_item.setInfo('video', addonitem.kodiInfo.video)
+            list_item.setArt(addonitem.kodiInfo.art)
+            if addonitem.kodiInfo.isPlayable:
+                list_item.setProperty('IsPlayable', 'true')
+            url = getUrl(action=addonitem.kodiInfo.action, productId=addonitem.npoInfo.productId, guid=addonitem.npoInfo.guid, slug=addonitem.npoInfo.slug)
+            xbmcplugin.addDirectoryItem(_handle, url, list_item, addonitem.kodiInfo.isFolder)        
+        # xbmcplugin.addSortMethod(
+        #     _handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     xbmcplugin.endOfDirectory(_handle)
 
-
-def list_overzicht():
-    xbmcplugin.setPluginCategory(_handle, 'Letters')
-    # Alle mogelijke "begin" "letters"
-    for letter in [
-        '0-9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-            'V', 'W', 'X', 'Y', 'Z']:
-        list_item = xbmcgui.ListItem(label=letter)
-        url = get_url(action='letter', letter=letter)
-        is_folder = True
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-    xbmcplugin.endOfDirectory(_handle)
-
-
-def list_franchise(link):
-    xbmcplugin.setPluginCategory(_handle, link)
-    # enable media info viewtype zodat "plot" van een aflevering ook getoond kan worden (indien gewenst)
-    xbmcplugin.setContent(_handle, 'movies')
-    episodesOrseason = uzg.episodesOrseason(link)
-    if (episodesOrseason['type'] == 'episodes'):
-        # het zijn gelijk afleveringen, pak de items en voeg deze toe!
-        add_video_items(episodesOrseason['items'])
-        # next 20 afleveringen
-        if (episodesOrseason['linknext'] is not None):
-            list_item = xbmcgui.ListItem(label='. -- ' + _addon.getLocalizedString(32003) + ' -- .')
-            list_item.setProperty('IsPlayable', 'false')
-            url = get_url(action='episodes', link=episodesOrseason['linknext'])
-            is_folder = True
-            xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-        xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_DATE)
-        xbmcplugin.addSortMethod(
-            _handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    else:
-        # het zijn seasons, we hebben dus een sub-level "seasons" nodig
-        add_season_items(episodesOrseason['items'])
-    xbmcplugin.endOfDirectory(_handle)
-
-
-def add_season_items(seasonitems):
-    for seasonitem in seasonitems:
-        # weergave seasons van franchise
-        list_item = xbmcgui.ListItem(label=seasonitem['label'])
-        url = get_url(action='episodes', link=seasonitem['link'])
-        is_folder = True
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-
-
-def add_video_items(videoitems):
-    for video in videoitems:
-        # weergave video's die kan direct onder een franchise zijn of vanaf een sub-level "seasons"
-        list_item = xbmcgui.ListItem(label=video['label'])
-        list_item.setInfo('video', video['video'])
-        list_item.setArt(video['art'])
-        list_item.setProperty('IsPlayable', 'true')
-        url = get_url(action='play', whatson_id=video['whatson_id'])
-        is_folder = False
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-
-
-def play_video(whatson_id):
-    subtitle = uzg.get_ondertitel(whatson_id)
-    stream_info = uzg.get_play_url(whatson_id)
-    playitem = xbmcgui.ListItem(path=stream_info['url'])
-
-    if (subtitle != None and xbmcplugin.getSetting(_handle, "subtitle") == 'true'):
-        playitem.setSubtitles([subtitle])
-    if (stream_info['drm']):
-        is_helper = inputstreamhelper.Helper(
-            PROTOCOL, DRM)  # drm=stream_info['drm'])
-        if is_helper.check_inputstream():
-            if (sys.version_info[0] == 3):
-                # Kodi 19
-                playitem.setProperty('inputstream','inputstream.adaptive')
-            else:
-                # Kodi 18
-                playitem.setProperty('inputstreamaddon', is_helper.inputstream_addon)
-            playitem.setProperty('inputstream.adaptive.manifest_type', PROTOCOL)
-            # stream_info['drm'])
-            playitem.setProperty('inputstream.adaptive.license_type', DRM)
-            playitem.setProperty('inputstream.adaptive.license_key', stream_info['license_key'])
-    xbmcplugin.setResolvedUrl(_handle, True, listitem=playitem)
-
+def playVideo(productId):
+    stream_info, licenseKey = uzg.getPlayInfo(productId)
+    playitem = xbmcgui.ListItem(path=stream_info["stream"]["streamURL"])
+    # xbmc.log('playVideo - {}'.format(productId),level=xbmc.LOGERROR)
+    is_helper = inputstreamhelper.Helper(PROTOCOL, DRM)
+    if is_helper.check_inputstream():
+        playitem.setProperty('inputstream','inputstream.adaptive')
+        playitem.setProperty('inputstream.adaptive.manifest_type', PROTOCOL)
+        playitem.setProperty('inputstream.adaptive.license_type', DRM)
+        if licenseKey:
+            playitem.setProperty('inputstream.adaptive.license_key', licenseKey)
+        xbmcplugin.setResolvedUrl(_handle, True, listitem=playitem)
 
 def router(paramstring):
     params = dict(parse_qsl(paramstring))
     if params:
-        if params['action'] == 'letter':
-            list_letter(params['letter'])
-        elif params['action'] == 'keuze':
-            list_keuze(params['keuze'])
-        elif params['action'] == 'episodes':
-            list_franchise(params['link'])
+        if params['action'] == 'keuze':
+            listKeuze(params['keuze'])
+        elif params['action'] == 'episodesSeason':
+            listEpisodesofSeason(params['guid'])
             setMediaView()
+        elif params['action'] == 'episodesSerie':
+            listEpisodesofSerie(params['guid'])
+            setMediaView()
+        elif params['action'] == 'seasons':
+            listSeasons(params['slug'])
         elif params['action'] == 'play':
-            play_video(params['whatson_id'])
+            playVideo(params['productId'])
         else:
             raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
     else:
-        home_menu()
+        homeMenu()
         setMediaView()
 
 
--- uzgtest.py
+++ uzgtest.py
@@ -1,16 +1,88 @@
 import resources.lib.uzg
+import resources.lib.npoapihelpers
 
 uzg = resources.lib.uzg.Uzg()
+npoapihelpers= resources.lib.npoapihelpers.NpoHelpers()
 
 #print(uzg.getAZPage('W'))
 
-#print(uzg.get_play_url('AT_2083054'))
-#print(uzg.get_play_url('POW_00398490'))
+# {
+# "guid": "2042e1ee-0e79-4766-aea2-5b300d6839b2",
+# "title": "NPO3",
+# "externalId": "LI_NL3_4188107"
+# },
 
-#print(uzg.episodesOrseason('https://start-api.npo.nl/page/franchise/VPWON_1260597'))
+a = npoapihelpers.getToken('LI_NL3_4188107')
 
-print(uzg.episodesOrseason('https://start-api.npo.nl/page/franchise/POW_03108581'))
+print(a) 
 
-#print(uzg.episodesOrseason('https://start-api.npo.nl/media/series/POW_03108581/episodes?seasonId=POW_03137258'))
+b = npoapihelpers.getStream(a)
 
-#print(uzg.episodesOrseason('https://start-api.npo.nl/page/franchise/NOSJournaal'))
+print(b)
+
+c = 3
+
+info, licenseKey = uzg.getPlayInfo('LI_NL3_4188107')
+
+a = npoapihelpers.getToken('VARA_101381121')
+
+print(a) 
+
+b = npoapihelpers.getStream(a)
+
+print(b)
+
+c = 3
+
+info, licenseKey = uzg.getPlayInfo('VARA_101381121')
+
+result = uzg.getChannels()
+
+print(uzg.getChannels())
+
+for item in result:
+    print(item.npoInfo.productId)
+    print(item.kodiInfo.isFolder)
+    print(item.kodiInfo.isPlayable)
+    print(item.kodiInfo.label)
+    print(item.kodiInfo.action)
+    print('---')
+
+print(uzg.getQueryPage('bijna'))
+
+result = uzg.getSeasons('we-zijn-er-bijna')
+
+for item in result:
+    print(item.npoInfo.slug)
+
+print(uzg.getSeasons('we-zijn-er-bijna'))
+
+result = uzg.getEpisodesOfSeason('d89e22bb-6983-49ac-8cad-faf539e5245e')
+
+for item in result:
+    print(item.npoInfo.productId)
+    print(item.kodiInfo.isFolder)
+    print(item.kodiInfo.isPlayable)
+    print(item.kodiInfo.label)
+    print(item.kodiInfo.video)
+    print('---')
+
+
+# https://npo.nl/start/api/domain/search-results?query=bijna&searchType=series&subscriptionType=premium&profileid=premium
+# https://npo.nl/start/api/domain/search-results?query=bijna&searchType=series&subscriptionType=anonymous
+# https://npo.nl/start/api/domain/series-detail?slug=we-zijn-er-bijna
+# https://npo.nl/start/api/domain/series-seasons?slug=we-zijn-er-bijna&type=timeless_series
+# https://npo.nl/start/api/domain/search-results?query=bijna&searchType=series&subscriptionType=anonymous
+# https://npo.nl/start/api/domain/programs-by-season?guid=7e1d457a-ec0f-4c25-853d-2085e55567b7&type=timeless_series
+# https://npo.nl/start/api/domain/series-seasons?slug=we-zijn-er-bijna&type=timeless_series
+# https://npo.nl/start/api/domain/programs-by-season?guid=d89e22bb-6983-49ac-8cad-faf539e5245e&type=timeless_series
+# https://npo.nl/start/api/domain/programs-by-series?seriesGuid=a9c7142c-d94e-4c13-ad2b-42e2772925aa&sort=-firstBroadcastDate
+# https://npo.nl/start/api/domain/programs-by-series?seriesGuid=a9c7142c-d94e-4c13-ad2b-42e2772925aa&limit=20&sort=firstBroadcastDate
+# https://npo.nl/start/api/domain/series-seasons?slug=we-zijn-er-bijna&type=timeless_series
+# https://npo.nl/start/api/domain/series-seasons?slug=we-zijn-er-bijna
+# https://npo.nl/start/api/domain/series-seasons?slug=we-zijn-er-bijna
+# https://npo.nl/start/_next/data/9gPO_EpYVoXUgPbn57qRY/serie/we-zijn-er-bijna.json?seriesSlug=we-zijn-er-bijna
+# https://npo.nl/start/_next/data/9gPO_EpYVoXUgPbn57qRY/serie/ik-durf-het-bijna-niet-te-vragen.json?seriesSlug=ik-durf-het-bijna-niet-te-vragen
+# https://npo.nl/start/_next/data/9gPO_EpYVoXUgPbn57qRY/categorie/series.json?slug=series
+# https://npo.nl/start/_next/data/9gPO_EpYVoXUgPbn57qRY/categorie/programmas.json?slug=programmas
+# https://npo.nl/start/api/domain/page-collection?guid=bcd8b931-c2df-4d53-9bcf-01faa4ac7050
